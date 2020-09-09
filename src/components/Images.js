2548b614d0ed99287c77316723ab25b42949619a
OnuR <onur.arslan@campus.tu-berlin.de>
2020-09-09 17:00:42 +0200

    - Kleie Anpassungen - Menü Leiste für Bilder hinzugefügt

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,12 +1,21 @@
 import React, {Component} from "react";
 
-export default class Home extends Component {
+export default class Images extends Component {
 
     render() {
         return (
             <div>
                 <main>
                     <h1>Resimler Bölümü</h1>
+                    <div>
+                        <ul className="list-unstyled">
+                            <li>2020 Yaz</li>
+                            <li>örnek 2</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
                 </main>
             </div>
         )
