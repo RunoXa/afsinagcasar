293805a7b46ea51a9e2fcf2be5614b9fc4ec147e
OnuR <onur.arslan@campus.tu-berlin.de>
2020-09-26 02:46:30 +0200

    - Radio als Beispiel eingebunden

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,48 +1,63 @@
 import React, {Component} from "react";
 import '../styles/Home.css';
 import Carousel from './Carousel'
+
 export default class Home extends Component {
+   componentDidMount() {
+      const script = document.createElement("script");
 
-    render() {
-        let heading = "AĞCAŞAR'A HOŞ GELDİNİZ";
-        let subheading1 = "Köroğlu dağının tepesinden aşağıya doğru hiç baktınız mı? Neden Köroğlu'na çıkarız? Neden Ağcaşar'a gideriz? Neden bütün sohbetlerimizde lafı mutlaka Ağcaşar'a getiririz? Neden sürekli birbirimizle sorunlar yaşarız? Neden sevdiklerimizden ayrı kalırız? Bir türlü kopamadığımız Ağcaşar'ı neden terk etmek zorunda kaldık? Zaman içerisinde neden çıkarcı olduk? Çok sevdiğimizi her vesileyle vurguladığımız Ağcaşar insanının yaptığı güzel şeyleri neden kıskanırız? Gücümüzü birleştirme konusunda neden duyarsızız? Neden ortak çıkarlarımız için yapılacak işlerin dışında dururuz? Geçmişimizi neden iyi bilmeyiz? Biz kimiz?";
-        let subheading2 = "Bu soruları çoğaltmak mümkün. Bizler bu ve buna benzer soruların cevabını ararken, dünyanın birçok yerine göçmüş Ağcaşarlılar arasında bir bağ kurmak istedik. Yaşam şartları öyle bir hal aldı ki çoğumuz köyümüzden, yöremizden  ayrılmak zorunda kaldık. Yaşadığımız bütün bu güzellikler yüreğimizin bir köşesinde sadece birer anı ve özlem olarak kaldı.";
-        let subheading3 = "İşte Ağcaşar Sitesi unutulmaya yüz tutmuş bu güzellikleri belleklerimizde yeniden canlandırmak, kültürel değerlerimize sahip çıkıp, gelecek nesillere taşımak, uzakları yakın kılmak, köyün sorunlarına yer vermek, bu sorunlara çözümler üretmek amacıyla 2006 yılının başında kurulmuştur.";
-        let subheading4 = "Bizler internet sayfası yapma konusunda profesyonel değiliz. Mutlaka yanlışlarımız olacaktır.";
-        let subheading5 = "Bu sayfada yayınlanmasını istediğiniz yazı ve resimleri bize gönderebilirsiniz.";
+      script.id = "radio-de-embedded";
+      script.src = "https://www.radio.de/inc/microsite/js/full.js";
+      script.async = true;
 
-        return (
-            <div className="home-content">
-                <section className="container">
-                    <Carousel/>
-                    {/*<Carousel>*/}
-                    {/*    <Carousel.Item>*/}
-                    {/*        <img className="home-image" src={HomeImage1}*/}
-                    {/*             alt="HomeImage1"/>*/}
-                    {/*    </Carousel.Item>*/}
-                    {/*    <Carousel.Item>*/}
-                    {/*        <img className="home-image" src={HomeImage2}*/}
-                    {/*             alt="HomeImage2"/>*/}
-                    {/*    </Carousel.Item>*/}
-                    {/*    <Carousel.Item>*/}
-                    {/*        <img className="home-image" src={HomeImage3}*/}
-                    {/*             alt="HomeImage3"/>*/}
-                    {/*    </Carousel.Item>*/}
-                    {/*</Carousel>*/}
-                    <h1 className="home-title">{heading}</h1>
-                    <div className="container">
-                        <div className="home-text">
-                            <p className="subtitle is-4">{subheading1}</p>
-                            <p className="subtitle is-4">{subheading2}</p>
-                            <p className="subtitle is-4">{subheading3}</p>
-                            <p className="subtitle is-4">{subheading4}</p>
-                            <p className="subtitle is-4">{subheading5}</p>
-                        </div>
-                        <a type="button" href="/" className="btn is-large text-dark bg-secondary" id="button">Coming
-                            Soon..</a>
-                    </div>
-                </section>
-            </div>
-        )
-    }
+      document.body.appendChild(script);
+   }
+
+   render() {
+      let heading = "AĞCAŞAR'A HOŞ GELDİNİZ";
+      let subheading1 = "Köroğlu dağının tepesinden aşağıya doğru hiç baktınız mı? Neden Köroğlu'na çıkarız? Neden Ağcaşar'a gideriz? Neden bütün sohbetlerimizde lafı mutlaka Ağcaşar'a getiririz? Neden sürekli birbirimizle sorunlar yaşarız? Neden sevdiklerimizden ayrı kalırız? Bir türlü kopamadığımız Ağcaşar'ı neden terk etmek zorunda kaldık? Zaman içerisinde neden çıkarcı olduk? Çok sevdiğimizi her vesileyle vurguladığımız Ağcaşar insanının yaptığı güzel şeyleri neden kıskanırız? Gücümüzü birleştirme konusunda neden duyarsızız? Neden ortak çıkarlarımız için yapılacak işlerin dışında dururuz? Geçmişimizi neden iyi bilmeyiz? Biz kimiz?";
+      let subheading2 = "Bu soruları çoğaltmak mümkün. Bizler bu ve buna benzer soruların cevabını ararken, dünyanın birçok yerine göçmüş Ağcaşarlılar arasında bir bağ kurmak istedik. Yaşam şartları öyle bir hal aldı ki çoğumuz köyümüzden, yöremizden  ayrılmak zorunda kaldık. Yaşadığımız bütün bu güzellikler yüreğimizin bir köşesinde sadece birer anı ve özlem olarak kaldı.";
+      let subheading3 = "İşte Ağcaşar Sitesi unutulmaya yüz tutmuş bu güzellikleri belleklerimizde yeniden canlandırmak, kültürel değerlerimize sahip çıkıp, gelecek nesillere taşımak, uzakları yakın kılmak, köyün sorunlarına yer vermek, bu sorunlara çözümler üretmek amacıyla 2006 yılının başında kurulmuştur.";
+      let subheading4 = "Bizler internet sayfası yapma konusunda profesyonel değiliz. Mutlaka yanlışlarımız olacaktır.";
+      let subheading5 = "Bu sayfada yayınlanmasını istediğiniz yazı ve resimleri bize gönderebilirsiniz.";
+
+      return (
+         <div className="home-content">
+            <section className="container">
+               <Carousel/>
+               {/*<Carousel>*/}
+               {/*    <Carousel.Item>*/}
+               {/*        <img className="home-image" src={HomeImage1}*/}
+               {/*             alt="HomeImage1"/>*/}
+               {/*    </Carousel.Item>*/}
+               {/*    <Carousel.Item>*/}
+               {/*        <img className="home-image" src={HomeImage2}*/}
+               {/*             alt="HomeImage2"/>*/}
+               {/*    </Carousel.Item>*/}
+               {/*    <Carousel.Item>*/}
+               {/*        <img className="home-image" src={HomeImage3}*/}
+               {/*             alt="HomeImage3"/>*/}
+               {/*    </Carousel.Item>*/}
+               {/*</Carousel>*/}
+               <h1 className="home-title">{heading}</h1>
+               <div className="container">
+                  <div className="home-text">
+                     <p className="subtitle is-4">{subheading1}</p>
+                     <p className="subtitle is-4">{subheading2}</p>
+                     <p className="subtitle is-4">{subheading3}</p>
+                     <p className="subtitle is-4">{subheading4}</p>
+                     <p className="subtitle is-4">{subheading5}</p>
+                  </div>
+                  <a type="button" href="/" className="btn is-large text-dark bg-secondary" id="button">Coming
+                     Soon..</a>
+               </div>
+            </section>
+            {/*<div className="ng-app-embedded">*/}
+            {/*   <div ui-view="" className="microsite embedded-radio-player" data-playerwidth="340px"*/}
+            {/*        data-playertype="web_embedded" data-playstation="turku" data-autoplay="true"*/}
+            {/*        data-apikey="df04ff67dd3339a6fc19c9b8be164d5b5245ae93"></div>*/}
+            {/*</div>*/}
+         </div>
+      )
+   }
 }
