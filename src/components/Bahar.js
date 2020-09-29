d210a22ec16ca395785a3a5f92d00f4ac4bf570e
OnuR <onur.arslan@campus.tu-berlin.de>
2020-09-29 16:14:30 +0200

    - Images von neu erstellt - hover für jedes Bild hinzugefügt

--- src/components/Bahar.js
+++ src/components/Bahar.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import '../styles/Images.css';
+import PropTypes from 'prop-types';
+import React from 'react';
 import Gallery from 'react-grid-gallery';
 import Image_Y_1 from '../images/koydeyasam/y_1.jpg';
 import Image_Y_2 from '../images/koydeyasam/y_2.jpg';
@@ -22,165 +22,203 @@ import Image_Y_18 from '../images/koydeyasam/y_18.jpg';
 import Image_Y_19 from '../images/koydeyasam/y_19.jpg';
 import Image_Y_20 from '../images/koydeyasam/y_20.jpg';
 
-const IMAGES =
-   [
-      {
-         src: Image_Y_1,
-         thumbnail: Image_Y_1,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_2,
-         thumbnail: Image_Y_2,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_3,
-         thumbnail: Image_Y_3,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_4,
-         thumbnail: Image_Y_4,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_5,
-         thumbnail: Image_Y_5,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_6,
-         thumbnail: Image_Y_6,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_7,
-         thumbnail: Image_Y_7,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_8,
-         thumbnail: Image_Y_8,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_9,
-         thumbnail: Image_Y_9,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_10,
-         thumbnail: Image_Y_10,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_11,
-         thumbnail: Image_Y_11,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_12,
-         thumbnail: Image_Y_12,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_13,
-         thumbnail: Image_Y_13,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_14,
-         thumbnail: Image_Y_14,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_15,
-         thumbnail: Image_Y_15,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_16,
-         thumbnail: Image_Y_16,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_17,
-         thumbnail: Image_Y_17,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_18,
-         thumbnail: Image_Y_18,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_19,
-         thumbnail: Image_Y_19,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-      {
-         src: Image_Y_20,
-         thumbnail: Image_Y_20,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
-      },
-   ];
 
-export default class Bahar extends Component {
+export default class Bahar extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         images: this.props.images
+      };
+   }
 
    render() {
-      return (
+      var images =
+         this.state.images.map((i) => {
+            i.customOverlay = (
+               <div style={captionStyle}>
 
-         <div className="main-images-1" style={{
+               </div>);
+            return i;
+         });
+
+
+      return (
+         <div style={{
             display: "block",
             minHeight: "1px",
             width: "100%",
-            overflow: "auto",
+            overflow: "auto"
          }}>
-            <div className="main-images">
-               <Gallery images={IMAGES} enableImageSelection={false}/>
-            </div>
+            <Gallery
+               images={images}
+               enableImageSelection={false}/>
          </div>
-      )
+      );
    }
 }
+
+Bahar.propTypes = {
+   images: PropTypes.arrayOf(
+      PropTypes.shape({
+         src: PropTypes.string.isRequired,
+         thumbnail: PropTypes.string.isRequired,
+         srcset: PropTypes.array,
+         caption: PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.element
+         ]),
+         thumbnailWidth: PropTypes.number.isRequired,
+         thumbnailHeight: PropTypes.number.isRequired
+      })
+   ).isRequired
+};
+
+const captionStyle = {
+   border: "90px solid rgba(46, 49, 49, 0.2)",
+}
+
+Bahar.defaultProps = {
+   images:
+      ([
+         {
+            src: Image_Y_1,
+            thumbnail: Image_Y_1,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: "Büyük Yenge - Köy"
+         },
+         {
+            src: Image_Y_2,
+            thumbnail: Image_Y_2,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_3,
+            thumbnail: Image_Y_3,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_4,
+            thumbnail: Image_Y_4,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_5,
+            thumbnail: Image_Y_5,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_6,
+            thumbnail: Image_Y_6,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_7,
+            thumbnail: Image_Y_7,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_8,
+            thumbnail: Image_Y_8,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_9,
+            thumbnail: Image_Y_9,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_10,
+            thumbnail: Image_Y_10,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_11,
+            thumbnail: Image_Y_11,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_12,
+            thumbnail: Image_Y_12,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_13,
+            thumbnail: Image_Y_13,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_14,
+            thumbnail: Image_Y_14,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_15,
+            thumbnail: Image_Y_15,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_16,
+            thumbnail: Image_Y_16,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_17,
+            thumbnail: Image_Y_17,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_18,
+            thumbnail: Image_Y_18,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_19,
+            thumbnail: Image_Y_19,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+         {
+            src: Image_Y_20,
+            thumbnail: Image_Y_20,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            caption: ""
+         },
+      ])
+};
