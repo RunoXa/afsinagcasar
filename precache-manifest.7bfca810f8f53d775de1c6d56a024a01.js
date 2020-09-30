3b9f82a7bf4c10e89b64ef2d0f0cc81ec2f4d7af
OnuR <onur.arslan@campus.tu-berlin.de>
2020-10-01 01:39:40 +0200

    Updates

---
+++ precache-manifest.7bfca810f8f53d775de1c6d56a024a01.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4018eac800a684282680753d5221ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb679d10d0b154665c65",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "ac64007ccc8d087fee84",
+    "url": "/static/css/main.f523c565.chunk.css"
+  },
+  {
+    "revision": "cb679d10d0b154665c65",
+    "url": "/static/js/2.2674d633.chunk.js"
+  },
+  {
+    "revision": "5747164133cb965579ca917c9e889fc4",
+    "url": "/static/js/2.2674d633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac64007ccc8d087fee84",
+    "url": "/static/js/main.ef7b4150.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "bca026585f2d4434bc937127dee3a3fa",
+    "url": "/static/media/aliriza.bca02658.jpg"
+  },
+  {
+    "revision": "899ff6f01c5d96cc0f7b368339683c95",
+    "url": "/static/media/bahar1.899ff6f0.JPG"
+  },
+  {
+    "revision": "88131389645f40be44c4bff8ac78a3bc",
+    "url": "/static/media/bahar10.88131389.JPG"
+  },
+  {
+    "revision": "d853a7fd0cbfb52f8f2444f37e99b22e",
+    "url": "/static/media/bahar11.d853a7fd.JPG"
+  },
+  {
+    "revision": "04be40b41eb70bb1bb048820a410d390",
+    "url": "/static/media/bahar12.04be40b4.JPG"
+  },
+  {
+    "revision": "79ec2c7fd51b99b9aea5fa0d059a8c67",
+    "url": "/static/media/bahar13.79ec2c7f.JPG"
+  },
+  {
+    "revision": "1bcf6ee75d18028a841c665fe9b725be",
+    "url": "/static/media/bahar14.1bcf6ee7.JPG"
+  },
+  {
+    "revision": "99c0ab4038af4fb0989c343ee7a83087",
+    "url": "/static/media/bahar15.99c0ab40.JPG"
+  },
+  {
+    "revision": "b1c5d46b746d81c3d2ac1ae9b2789330",
+    "url": "/static/media/bahar16.b1c5d46b.JPG"
+  },
+  {
+    "revision": "a6f7206a657fc46ce2b8bee72d5ddb6a",
+    "url": "/static/media/bahar17.a6f7206a.JPG"
+  },
+  {
+    "revision": "d5f3fb61dbb80591d84e02946b3f7349",
+    "url": "/static/media/bahar18.d5f3fb61.JPG"
+  },
+  {
+    "revision": "ff4431085e0cec4a6cb2db04a77dd385",
+    "url": "/static/media/bahar19.ff443108.JPG"
+  },
+  {
+    "revision": "777987e81e3c7078b117329de24eee40",
+    "url": "/static/media/bahar2.777987e8.JPG"
+  },
+  {
+    "revision": "6a0f1b25596fff9fcfc7b2e089d29128",
+    "url": "/static/media/bahar20.6a0f1b25.JPG"
+  },
+  {
+    "revision": "ca684d8b6c3b27f74081566bbd1e454d",
+    "url": "/static/media/bahar21.ca684d8b.JPG"
+  },
+  {
+    "revision": "417c050fe48b70194bc307b10fd6659f",
+    "url": "/static/media/bahar22.417c050f.JPG"
+  },
+  {
+    "revision": "7025dd368673bbf10b947cf8eb7fae99",
+    "url": "/static/media/bahar23.7025dd36.JPG"
+  },
+  {
+    "revision": "e9ada91a7378ee5be47de2f6d5be0578",
+    "url": "/static/media/bahar24.e9ada91a.JPG"
+  },
+  {
+    "revision": "37f174f8d2f30a76b98f6a3724b1d976",
+    "url": "/static/media/bahar25.37f174f8.JPG"
+  },
+  {
+    "revision": "350e99a9ca86145c5bae93530b507042",
+    "url": "/static/media/bahar26.350e99a9.JPG"
+  },
+  {
+    "revision": "308ae2c875f9a37a4dbbfd0f27525e13",
+    "url": "/static/media/bahar27.308ae2c8.JPG"
+  },
+  {
+    "revision": "b1f538d7ae674ae3d25dd029212286dc",
+    "url": "/static/media/bahar28.b1f538d7.JPG"
+  },
+  {
+    "revision": "f5e80f49d0dd7309225d3a432277de60",
+    "url": "/static/media/bahar29.f5e80f49.JPG"
+  },
+  {
+    "revision": "9fbb1600365c1fa1ccabb8bab5cc14de",
+    "url": "/static/media/bahar3.9fbb1600.JPG"
+  },
+  {
+    "revision": "79135ee781727be2a0cff4514be0d25a",
+    "url": "/static/media/bahar30.79135ee7.JPG"
+  },
+  {
+    "revision": "bd6ac9b8b96ff4d6bbc369dfe149e0fb",
+    "url": "/static/media/bahar4.bd6ac9b8.JPG"
+  },
+  {
+    "revision": "00ceedb4decd193aabe857ce48b0a76c",
+    "url": "/static/media/bahar5.00ceedb4.JPG"
+  },
+  {
+    "revision": "1705bd2dde8cde80ae91ff5832e96e95",
+    "url": "/static/media/bahar6.1705bd2d.JPG"
+  },
+  {
+    "revision": "1f53f32ad139e130d95b1711f7a892e4",
+    "url": "/static/media/bahar7.1f53f32a.JPG"
+  },
+  {
+    "revision": "97dd2de2a44a96b78ca27c9ccae3d7dd",
+    "url": "/static/media/bahar8.97dd2de2.JPG"
+  },
+  {
+    "revision": "c9deb581d8db2430da3e040331ea3429",
+    "url": "/static/media/bahar9.c9deb581.JPG"
+  },
+  {
+    "revision": "668075fb037300f702b3150ee6295a97",
+    "url": "/static/media/culture.668075fb.svg"
+  },
+  {
+    "revision": "415812b12c9fd5865879ee9681f3781d",
+    "url": "/static/media/diagram.415812b1.svg"
+  },
+  {
+    "revision": "b81b1124bb3c1c707522838976ca9387",
+    "url": "/static/media/guzel.b81b1124.jpg"
+  },
+  {
+    "revision": "4fb154edf17fc56ffe6b5e1b5b73ea61",
+    "url": "/static/media/home-image1.4fb154ed.jpg"
+  },
+  {
+    "revision": "9bb362f35107e7ffb9b2ca6be41767bb",
+    "url": "/static/media/home-image2.9bb362f3.jpg"
+  },
+  {
+    "revision": "596b02af835c209ea4a25e06a8ebe77e",
+    "url": "/static/media/home-image3.596b02af.jpg"
+  },
+  {
+    "revision": "ae2d1d64548ceccf40e61d3ca2c618d4",
+    "url": "/static/media/house.ae2d1d64.svg"
+  },
+  {
+    "revision": "dd9674bc6eb2c32088b884376b27b476",
+    "url": "/static/media/huseyinsumbul.dd9674bc.jpg"
+  },
+  {
+    "revision": "f571911bcafaa076cbd3a2dd289bb5b1",
+    "url": "/static/media/images.f571911b.svg"
+  },
+  {
+    "revision": "7f2e9f62059aaafbf85d2ba77dcd372d",
+    "url": "/static/media/senemakgoz.7f2e9f62.jpg"
+  },
+  {
+    "revision": "3a30e59f8f5158513d289762db4ab0fc",
+    "url": "/static/media/sengul.3a30e59f.jpg"
+  },
+  {
+    "revision": "676df565e2ff00ea631fe4f4dd6f6c6f",
+    "url": "/static/media/text.676df565.svg"
+  },
+  {
+    "revision": "f1d3e6fb8c6948f38dc548dde5ecaa36",
+    "url": "/static/media/y_1.f1d3e6fb.jpg"
+  },
+  {
+    "revision": "5e466432d7f805e4b0820b2f1bad7a89",
+    "url": "/static/media/y_10.5e466432.jpg"
+  },
+  {
+    "revision": "e1425a3cee946951f79cc31de62d15a4",
+    "url": "/static/media/y_11.e1425a3c.jpg"
+  },
+  {
+    "revision": "1862c411dae5d80bfeecc4aee2a388c8",
+    "url": "/static/media/y_12.1862c411.jpg"
+  },
+  {
+    "revision": "8fab47df46c8b63741b744b6dfd0749e",
+    "url": "/static/media/y_13.8fab47df.jpg"
+  },
+  {
+    "revision": "aaca690ecb3fb213fb7efff7176b650b",
+    "url": "/static/media/y_14.aaca690e.jpg"
+  },
+  {
+    "revision": "59b7040f9ae7e32d28d873b550edd9ce",
+    "url": "/static/media/y_15.59b7040f.jpg"
+  },
+  {
+    "revision": "44a2f88529ea7c90a9533d4fc1c15005",
+    "url": "/static/media/y_16.44a2f885.jpg"
+  },
+  {
+    "revision": "a12a556a3ca54f66ed9741b6752d3c68",
+    "url": "/static/media/y_17.a12a556a.jpg"
+  },
+  {
+    "revision": "3d9029821711c53f66c63f250a636585",
+    "url": "/static/media/y_18.3d902982.jpg"
+  },
+  {
+    "revision": "55e88b2f068bb9c8c1c76cd963869a82",
+    "url": "/static/media/y_19.55e88b2f.jpg"
+  },
+  {
+    "revision": "59c2c75dca37076e24c97f05c81d004b",
+    "url": "/static/media/y_2.59c2c75d.jpg"
+  },
+  {
+    "revision": "aad527206bb250c86ac642d48b7a1908",
+    "url": "/static/media/y_20.aad52720.jpg"
+  },
+  {
+    "revision": "f0d69bbe5aa51b65db6c40f361421a11",
+    "url": "/static/media/y_3.f0d69bbe.jpg"
+  },
+  {
+    "revision": "dfa48f10f427c1a8158722bb0ddbe9ed",
+    "url": "/static/media/y_4.dfa48f10.jpg"
+  },
+  {
+    "revision": "117631fbd32decd48275974f2394b088",
+    "url": "/static/media/y_5.117631fb.jpg"
+  },
+  {
+    "revision": "0500d1e1f21b77c85bd4a87fec7d0b43",
+    "url": "/static/media/y_6.0500d1e1.jpg"
+  },
+  {
+    "revision": "1da021a50639458e279b6c97fcafd514",
+    "url": "/static/media/y_7.1da021a5.jpg"
+  },
+  {
+    "revision": "c32043f924540aecaa11a72c9bad9901",
+    "url": "/static/media/y_8.c32043f9.jpg"
+  },
+  {
+    "revision": "7e9094b2d2fe60be7e7050dfa20801fc",
+    "url": "/static/media/y_9.7e9094b2.jpg"
+  },
+  {
+    "revision": "a40ade4cd7e7e6a01d7a01bf7ae13041",
+    "url": "/static/media/yayla1.a40ade4c.jpg"
+  },
+  {
+    "revision": "6bd4b01f5949fc16a739742204009ba2",
+    "url": "/static/media/yayla2.6bd4b01f.jpg"
+  },
+  {
+    "revision": "f9dd62792ca1921f2654934aaf296b01",
+    "url": "/static/media/yayla3.f9dd6279.jpg"
+  },
+  {
+    "revision": "602f37295f14af87764b555166bf51d0",
+    "url": "/static/media/youtube.602f3729.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/afsinagcasar/precache-manifest.af6fedecf8a41d30f787d9ed386ce044.js"
+  "/precache-manifest.7bfca810f8f53d775de1c6d56a024a01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/afsinagcasar/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
