891ddbbec8d00a7f4066b6aaf8899f2dd5bc4866
OnuR <onur.arslan@campus.tu-berlin.de>
2020-10-04 23:21:15 +0200

    - css für den image ausgelagert

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -12,9 +12,7 @@ export default class NotFound extends Component {
                   <h1 className="notFoundTitle">Sayfa Bulunamadı</h1>
                   <div className="container">
                      <div className="notFoundText">
-                        <img src={NotFoundImage} alt="NotFoundImage" style={{
-                           maxWidth: "300px", marginRight: "auto", marginLeft: "auto", width: "50%", display: "block"
-                        }}/>
+                        <img src={NotFoundImage} alt="NotFoundImage" className="notFoundImage"/>
                         <p><h4 className="notFoundTitle">ANASAYFAYA
                            <a href="/" className="linkToHomepage">GERİ DÖN</a>
                         </h4></p>
