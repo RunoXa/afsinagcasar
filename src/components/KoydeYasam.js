a7bba5f516d5f4209279bc390557434790e29cc3
OnuR <onur.arslan@campus.tu-berlin.de>
2020-10-20 14:49:47 +0200

    - Neue Bilder Galerie ausprobiert

--- src/components/KoydeYasam.js
+++ src/components/KoydeYasam.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import '../styles/Images.css';
 import Gallery from 'react-grid-gallery';
+import Lightbox from 'react-lightbox-component';
+import 'react-lightbox-component/build/css/index.css';
 import Image_Y_1 from '../images/koydeyasam/y_1.jpg';
 import Image_Y_2 from '../images/koydeyasam/y_2.jpg';
 import Image_Y_3 from '../images/koydeyasam/y_3.jpg';
@@ -22,161 +24,269 @@ import Image_Y_18 from '../images/koydeyasam/y_18.jpg';
 import Image_Y_19 from '../images/koydeyasam/y_19.jpg';
 import Image_Y_20 from '../images/koydeyasam/y_20.jpg';
 
-const IMAGES =
+// const IMAGES =
+//    [
+//       {
+//          src: Image_Y_1,
+//          thumbnail: Image_Y_1,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: "Büyük Yenge"
+//       },
+//       {
+//          src: Image_Y_2,
+//          thumbnail: Image_Y_2,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_3,
+//          thumbnail: Image_Y_3,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_4,
+//          thumbnail: Image_Y_4,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_5,
+//          thumbnail: Image_Y_5,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_6,
+//          thumbnail: Image_Y_6,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_7,
+//          thumbnail: Image_Y_7,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_8,
+//          thumbnail: Image_Y_8,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_9,
+//          thumbnail: Image_Y_9,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_10,
+//          thumbnail: Image_Y_10,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_11,
+//          thumbnail: Image_Y_11,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_12,
+//          thumbnail: Image_Y_12,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_13,
+//          thumbnail: Image_Y_13,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_14,
+//          thumbnail: Image_Y_14,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_15,
+//          thumbnail: Image_Y_15,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_16,
+//          thumbnail: Image_Y_16,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_17,
+//          thumbnail: Image_Y_17,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_18,
+//          thumbnail: Image_Y_18,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_19,
+//          thumbnail: Image_Y_19,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//       {
+//          src: Image_Y_20,
+//          thumbnail: Image_Y_20,
+//          thumbnailWidth: 320,
+//          thumbnailHeight: 212,
+//          caption: ""
+//       },
+//    ];
+
+const images =
    [
       {
          src: Image_Y_1,
-         thumbnail: Image_Y_1,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: "Büyük Yenge"
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_2,
-         thumbnail: Image_Y_2,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_3,
-         thumbnail: Image_Y_3,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_4,
-         thumbnail: Image_Y_4,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_5,
-         thumbnail: Image_Y_5,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_6,
-         thumbnail: Image_Y_6,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_7,
-         thumbnail: Image_Y_7,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_8,
-         thumbnail: Image_Y_8,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_9,
-         thumbnail: Image_Y_9,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_10,
-         thumbnail: Image_Y_10,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_11,
-         thumbnail: Image_Y_11,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_12,
-         thumbnail: Image_Y_12,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_13,
-         thumbnail: Image_Y_13,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_14,
-         thumbnail: Image_Y_14,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_15,
-         thumbnail: Image_Y_15,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_16,
-         thumbnail: Image_Y_16,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_17,
-         thumbnail: Image_Y_17,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_18,
-         thumbnail: Image_Y_18,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: '',
+         description: ''
       },
       {
          src: Image_Y_19,
-         thumbnail: Image_Y_19,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
       {
          src: Image_Y_20,
-         thumbnail: Image_Y_20,
-         thumbnailWidth: 320,
-         thumbnailHeight: 212,
-         caption: ""
+         title: 'image title',
+         description: 'image description'
       },
-   ];
+   ]
+
 
 export default class KoydeYasam extends Component {
 
    render() {
       return (
-
          <div className="main-images-1" style={{
             display: "block",
             marginTop: "60px",
             minHeight: "1px",
          }}>
-            <Gallery images={IMAGES} enableImageSelection={false} showLightboxThumbnails={true}/>
+            <Lightbox
+               images={images}
+               thumbnailWidth='320px'
+               thumbnailHeight='212px'
+               showImageModifiers={false}/>
          </div>
       )
    }
