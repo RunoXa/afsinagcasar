b28e3afa6bcc032fba7b847f1f2e67ea35c55f3d
OnuR <onur.arslan@campus.tu-berlin.de>
2020-12-26 02:46:05 +0100

    - Chatten implementiert

--- src/components/chat/ChatContent.js
+++ src/components/chat/ChatContent.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
 import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
 import ListItemText from '@material-ui/core/ListItemText';
 import Fab from '@material-ui/core/Fab';
 import SendIcon from '@material-ui/icons/Send';
@@ -91,24 +92,60 @@ const useStyles = makeStyles({
 export default function ChatContent() {
    const classes = useStyles();
    const [chatConversation, setChatConversation] = useState([]);
+   const [currentFirstname, setCurrentUserFirstName] = useState([]);
+   const [currentLastname, setCurrentUserLastName] = useState([]);
+
+   const submitMessage = useCallback(async event => {
+      event.preventDefault();
+      const {message} = event.target.elements;
+      try {
+         await firestore.collection('chat').add({
+            firstName: currentFirstname,
+            lastName: currentLastname,
+            message: message.value,
+            created: new Date(),
+            user_id: auth.currentUser.uid
+         });
+      } catch (error) {
+         console.log("Error adding document to collection:", error);
+      }
+   }, [currentFirstname, currentLastname]);
 
    useEffect(() => {
+      //get username
+      if (auth.currentUser != null) {
+         firestore.collection("users")
+            .doc(auth.currentUser.uid)
+            .get()
+            .then(doc => {
+               if (doc.exists) {
+                  const data = doc.data();
+                  setCurrentUserFirstName(data.firstName);
+                  setCurrentUserLastName(data.lastName);
+               } else {
+                  alert("No such document!");
+               }
+            }).catch(function (error) {
+            console.log("Error getting document:", error);
+         });
+      }
+
+      //get chat messages
       firestore.collection("chat")
          .onSnapshot((snapshot) => {
             const conversation = [];
             snapshot.forEach((doc) => conversation.push({...doc.data()}));
             setChatConversation(conversation);
          });
-   }, []);
 
-   console.log()
+   }, []);
 
    return (
       <div>
          <Grid container className={classes.chatSection}>
             <Grid item xs={12}>
-               <div className={classes.messageArea}>
-                  {chatConversation.sort((a, b) => a.createdAt > b.createdAt ? 1 : -1).map(function (bubbleData, i) {
+               <List className={classes.messageArea}>
+                  {chatConversation.sort((a, b) => a.created > b.created ? 1 : -1).map(function (bubbleData, i) {
                      return (
                         bubbleData.user_id === auth.currentUser.uid ?
                            <ListItem>
@@ -117,7 +154,8 @@ export default function ChatContent() {
                                     <ListItemText className={classes.messageBoxRight} align="right">
                                        <div className={classes.text}>
                                           <p className={classes.currentUserName}>{bubbleData.firstName + ' ' + bubbleData.lastName}<span
-                                             className={classes.timestamp}> (10:40)</span></p>
+                                             className={classes.timestamp}> {new Date(bubbleData.created.toDate()).toDateString()}</span>
+                                          </p>
                                           <p className={classes.message}>{bubbleData.message}</p>
                                        </div>
                                     </ListItemText>
@@ -131,7 +169,8 @@ export default function ChatContent() {
                                     <ListItemText className={classes.messageBoxLeft} align="left">
                                        <div className={classes.text}>
                                           <p className={classes.otherUserName}>{bubbleData.firstName + ' ' + bubbleData.lastName}<span
-                                             className={classes.timestamp}> (00:15)</span></p>
+                                             className={classes.timestamp}> {new Date(bubbleData.created.toDate()).toDateString()}</span>
+                                          </p>
                                           <p className={classes.message}>{bubbleData.message}</p>
                                        </div>
                                     </ListItemText>
@@ -140,16 +179,19 @@ export default function ChatContent() {
                            </ListItem>
                      )
                   })}
-               </div>
+               </List>
                <Divider/>
-               <Grid container style={{padding: '20px'}}>
-                  <Grid item xs={11}>
-                     <TextField id="outlined-basic-email" label="Type Something" fullWidth/>
+               <form className={classes.form} onSubmit={submitMessage}>
+                  <Grid container style={{padding: '20px'}}>
+                     <Grid item xs={11}>
+                        <TextField id="message" label="Mesaj yaz.." fullWidth name="message" type="message"/>
+                     </Grid>
+                     <Grid xs={1} align="right">
+                        <Fab type="submit" color="primary" size="small" aria-label="add"
+                             className={classes.fab}><SendIcon/></Fab>
+                     </Grid>
                   </Grid>
-                  <Grid xs={1} align="right">
-                     <Fab color="primary" size="small" aria-label="add" className={classes.fab}><SendIcon/></Fab>
-                  </Grid>
-               </Grid>
+               </form>
             </Grid>
          </Grid>
       </div>
