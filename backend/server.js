c794f481ee81334cf73b9a76445639fa88f56e71
OnuR <onur.arslan@campus.tu-berlin.de>
2021-01-10 14:15:16 +0100

    - Einstellungen für heroku

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('dist'));
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3333;
 
 console.log(PORT);
 
